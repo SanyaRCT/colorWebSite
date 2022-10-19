d3216ff703d70c05906cee66dd67709243927e1d
Oleksandr Turianytsia <o.turianytsia@gmail.com>
2022-10-19 14:05:31 +0300

    colorWebSite2

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 const cols = document.querySelectorAll(".col");
 
+document.addEventListener("keydown", (event) => {
+  event.preventDefault();
+  if (event.code.toLocaleLowerCase() === "space") {
+    setRendomColors();
+  }
+});
+
+document.addEventListener("click", (event) => {
+  const type = event.target.dataset.type;
+
+  if (type === "lock") {
+    const node =
+      event.target.tagName.toLocaleLowerCase() === "i"
+        ? event.target
+        : event.target.children[0];
+
+    node.classList.toggle("fa-lock-open");
+    node.classList.toggle("fa-lock");
+  } else if (type === "copy") {
+    copyToClickboard(event.target.textContent);
+  }
+});
+
 function generateRandomColor() {
   const hexCodes = "123456789ABCDEF";
   let color = "";
@@ -9,20 +32,61 @@ function generateRandomColor() {
   return "#" + color;
 }
 
-function setRendomColors() {
-  cols.forEach((col) => {
+function copyToClickboard(text) {
+  return navigator.clipboard.writeText(text);
+}
+
+function setRendomColors(isInitial) {
+  const colors = isInitial ? getColorsFromHash() : [];
+  cols.forEach((col, index) => {
+    const isLocked = col.querySelector("i").classList.contains("fa-lock");
     const text = col.querySelector("h2");
     const button = col.querySelector("button");
-    const color = generateRandomColor();
+    const color = isInitial
+      ? colors[index]
+        ? colors[index]
+        : generateRandomColor()
+      : generateRandomColor();
+
+    if (isLocked) {
+      colors.push(text.textContent);
+      return;
+    }
+
+    if (!isInitial) {
+      colors.push(color);
+    }
+
     text.textContent = color;
     col.style.background = color;
     setTextColor(text, color);
     setTextColor(button, color);
   });
+
+  updateColorsHash(colors);
 }
 
 function setTextColor(text, color) {
   const luminance = chroma(color).luminance();
   text.style.color = luminance > 0.5 ? "black" : "white";
 }
-setRendomColors();
+
+function updateColorsHash(colors = []) {
+  document.location.hash = colors
+    .map((col) => {
+      return col.toString().substring(1);
+    })
+    .join("-");
+}
+
+function getColorsFromHash() {
+  if (document.location.hash.length > 1) {
+    return document.location.hash
+      .substring(1)
+      .split("-")
+      .map((color) => "#" + color);
+  }
+  return [];
+}
+
+setRendomColors(true);
